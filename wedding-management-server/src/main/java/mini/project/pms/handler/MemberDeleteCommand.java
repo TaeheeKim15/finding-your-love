f6600f42e3ae6f95b702cccb39dccd7eedf378e4
TaeheeKim15 <utility_maker@naver.com>
2020-10-22 11:21:10 +0900

    .

--- wedding-management-server/src/main/java/mini/project/pms/handler/MemberDeleteCommand.java
+++ wedding-management-server/src/main/java/mini/project/pms/handler/MemberDeleteCommand.java
@@ -3,50 +3,51 @@ package mini.project.pms.handler;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.List;
+
 import mini.project.pms.domain.Member;
 import mini.project.util.Prompt;
 
 public class MemberDeleteCommand implements Command {
+	List<Member> memberList;
 
-  List<Member> memberList;
+	public MemberDeleteCommand(List<Member> list) {
+		this.memberList = list;
+	}
 
-  public MemberDeleteCommand(List<Member> list) {
-    this.memberList = list;
-  }
+	@Override
+	public void execute(PrintWriter out, BufferedReader in) {
+		try {
+			out.println("[회원 삭제]");
+			String id = Prompt.inputString("아이디를 입력해주세요 : ", out, in);
+			int index = indexOf(id);
 
-  @Override
-  public void execute(PrintWriter out, BufferedReader in) {
-    try {
-      out.println("[회원 삭제]");
-      int no = Prompt.inputInt("번호? ", out, in);
-      int index = indexOf(no);
+			if (index == -1) {
+				out.println("해당 번호의 회원이 없습니다.");
+				return;
+			}
 
-      if (index == -1) {
-        out.println("해당 번호의 회원이 없습니다.");
-        return;
-      }
+			String response = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ", out, in);
+			if (!response.equalsIgnoreCase("y")) {
+				out.println("회원 삭제를 취소하였습니다.");
+				return;
+			}
 
-      String response = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ", out, in);
-      if (!response.equalsIgnoreCase("y")) {
-        out.println("회원 삭제를 취소하였습니다.");
-        return;
-      }
+			memberList.remove(index);
+			out.println("회원을 삭제하였습니다.");
+			out.println("그동안 Finding your love...S2 를 이용해주셔서 감사합니다.");
 
-      memberList.remove(index);
-      out.println("회원을 삭제하였습니다.");
+		} catch (Exception e) {
+			out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
+		}
+	}
 
-    } catch (Exception e) {
-      out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
-    }
-  }
-
-  private int indexOf(int no) {
-    for (int i = 0; i < memberList.size(); i++) {
-      Member member = memberList.get(i);
-      if (member.getNo() == no) {
-        return i;
-      }
-    }
-    return -1;
-  }
+	private int indexOf(String id) {
+		for (int i = 0; i < memberList.size(); i++) {
+			Member member = memberList.get(i);
+			if (member.getId().equals(id)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
