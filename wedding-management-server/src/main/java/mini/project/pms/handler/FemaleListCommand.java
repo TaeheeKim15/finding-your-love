19ee7dd1fd4935f554aebd94f77d304bf7d28c32
jiyounyou <younny418@gmail.com>
2020-10-22 12:21:21 +0900

    backup

--- wedding-management-server/src/main/java/mini/project/pms/handler/FemaleListCommand.java
+++ wedding-management-server/src/main/java/mini/project/pms/handler/FemaleListCommand.java
@@ -4,15 +4,14 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
-
 import mini.project.pms.domain.Member;
 
 public class FemaleListCommand implements Command {
 
-	List<Member> femaleList;
+	List<Member> memberList;
 
 	public FemaleListCommand(List<Member> list) {
-		this.femaleList = list;
+		this.memberList = list;
 	}
 
 	@Override
@@ -21,26 +20,17 @@ public class FemaleListCommand implements Command {
 
 		// 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
 		// 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
-		Iterator<Member> iterator = femaleList.iterator();
+		Iterator<Member> iterator = memberList.iterator();
 
 		while (iterator.hasNext()) {
 			Member member = iterator.next();
-			out.printf("%s, %d, %s\n",
-					member.getName(),
-					member.getAge(),
-					member.getHobby());
-		}
-	}
-
-
-	public Member findByName(String name) {
-		for (int i = 0; i < femaleList.size(); i++) {
-			Member femaleMember = femaleList.get(i);
-			if (femaleMember.getName().equals(name) && femaleMember.getGender() == 1) {
-				return femaleMember;
+			if (member.getGender() == 1) {
+			  out.printf("%s, %d, %s\n",
+			      member.getName(),
+			      member.getAge(),
+			      member.getHobby());
 			}
 		}
-		return null;
 	}
 
 }
