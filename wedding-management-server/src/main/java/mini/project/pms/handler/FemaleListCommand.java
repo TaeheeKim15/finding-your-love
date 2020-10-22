275513da71a45c8d5cac5455ba58035485351d81
jiyounyou <younny418@gmail.com>
2020-10-22 12:22:23 +0900

    backup

--- wedding-management-server/src/main/java/mini/project/pms/handler/FemaleListCommand.java
+++ wedding-management-server/src/main/java/mini/project/pms/handler/FemaleListCommand.java
@@ -8,29 +8,29 @@ import mini.project.pms.domain.Member;
 
 public class FemaleListCommand implements Command {
 
-	List<Member> memberList;
+  List<Member> memberList;
 
-	public FemaleListCommand(List<Member> list) {
-		this.memberList = list;
-	}
+  public FemaleListCommand(List<Member> list) {
+    this.memberList = list;
+  }
 
-	@Override
-	public void execute(PrintWriter out, BufferedReader in) {
-		out.println("[여성회원 목록]");
+  @Override
+  public void execute(PrintWriter out, BufferedReader in) {
+    out.println("[여성회원 목록]");
 
-		// 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
-		// 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
-		Iterator<Member> iterator = memberList.iterator();
+    // 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
+    // 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
+    Iterator<Member> iterator = memberList.iterator();
 
-		while (iterator.hasNext()) {
-			Member member = iterator.next();
-			if (member.getGender() == 1) {
-			  out.printf("%s, %d, %s\n",
-			      member.getName(),
-			      member.getAge(),
-			      member.getHobby());
-			}
-		}
-	}
+    while (iterator.hasNext()) {
+      Member member = iterator.next();
+      if (member.getGender() == 1) {
+        out.printf("%s, %d, %s\n",
+            member.getName(),
+            member.getAge(),
+            member.getHobby());
+      }
+    }
+  }
 
 }
--- wedding-management-server/src/main/java/mini/project/pms/handler/MaleListCommand.java
+++ wedding-management-server/src/main/java/mini/project/pms/handler/MaleListCommand.java
@@ -4,42 +4,33 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
-
 import mini.project.pms.domain.Member;
 
 public class MaleListCommand implements Command {
 
-	List<Member> maleList;
+  List<Member> memberList;
 
-	public MaleListCommand(List<Member> list) {
-		this.maleList = list;
-	}
+  public MaleListCommand(List<Member> list) {
+    this.memberList = list;
+  }
 
-	@Override
-	public void execute(PrintWriter out, BufferedReader in) {
-		out.println("[남성 회원 목록]");
+  @Override
+  public void execute(PrintWriter out, BufferedReader in) {
+    out.println("[남성 회원 목록]");
 
-		// 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
-		// 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
-		Iterator<Member> iterator = maleList.iterator();
+    // 전체 목록을 조회할 때 `Iterator` 객체를 사용한다.
+    // 만약 목록의 일부만 조회하면다면 인덱스를 직접 다루는 이전 방식을 사용해야 한다.
+    Iterator<Member> iterator = memberList.iterator();
 
-		while (iterator.hasNext()) {
-			Member maleMember = iterator.next();
-			out.printf("%s, %d, %s\n",
-					maleMember.getName(),
-					maleMember.getAge(),
-					maleMember.getHobby());
-		}
-	}
-
-	public Member findByName(String name) {
-		for (int i = 0; i < maleList.size(); i++) {
-			Member maleMember = maleList.get(i);
-			if (maleMember.getName().equals(name) && maleMember.getGender() == 2) {
-				return maleMember;
-			}
-		}
-		return null;
-	}
+    while (iterator.hasNext()) {
+      Member member = iterator.next();
+      if (member.getGender() == 2) {
+        out.printf("%s, %d, %s\n",
+            member.getName(),
+            member.getAge(),
+            member.getHobby());
+      }
+    }
+  }
 
 }
